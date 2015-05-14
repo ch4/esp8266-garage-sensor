661f54ba7179169727dcc97dbbec63d058d6de1d
Harvey <ch4ch4@gmail.com>
2015-05-14 13:29:39 -0700

    :cake:

---
+++ configServerInit.lua
@@ -0,0 +1,13 @@
+apNamePrefix = "ConfigureMe" 
+
+apNetConfig = {ip      = "192.168.4.1", 
+               netmask = "255.255.255.0",
+               gateway = "192.168.4.1"}
+
+local apName = apNamePrefix .. "-" .. string.sub(wifi.ap.getmac(),13)
+print("Starting up AP with SSID: " .. apName);
+wifi.setmode(wifi.STATIONAP)
+local apSsidConfig = {}
+apSsidConfig.ssid = apName
+wifi.ap.config(apSsidConfig)
+wifi.ap.setip(apNetConfig)
